15f76ece85fc37092f346f7342986b18646db9bf
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-27 00:32:17 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        if (lastPrinted > 0){
+            int n = 1;
+            System.out.println(n);
+            do {
+                n++;
+                System.out.println(n);
+                n += 2;
+                System.out.println(n);
+            }   while(n < lastPrinted);
+        }
     }
 }
